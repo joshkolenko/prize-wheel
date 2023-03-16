94aef368e340b06d94ec7f86aca52d5b797407ab
Josh Kolenko <95929281+joshkolenko@users.noreply.github.com>
2023-03-16 14:32:36 -0400

    feat: add spin and weighted result functionality

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function createWheel(el, options) {
     el = document.querySelector(el);
   }
 
-  console.log(options.stroke);
-
   options = {
-    duration: 8000,
+    counterclockwise: false,
+    duration: 6000,
     timing: 'ease',
     size: 100,
     scale: 3,
+    speed: 1,
     ...options,
     stroke:
       options.stroke === false
@@ -21,9 +21,9 @@ function createWheel(el, options) {
           },
     text: {
       color: 'black',
-      font: 'Arial, Helvetica, sans-serif',
-      offset: 15,
-      size: 20,
+      font: 'Arial, Helvetica',
+      offset: 12,
+      size: 16,
       style: 'normal',
       ...options.text,
     },
@@ -32,6 +32,17 @@ function createWheel(el, options) {
   if (options.stroke === false) {
     options.stroke = { color: 'transparent', width: 0 };
   }
+
+  options.segments.map((segment, i) => {
+    return {
+      ...segment,
+      size: 360 / options.segments.length,
+      angles: {
+        start: length * i,
+        end: length * i + length,
+      },
+    };
+  });
 
   let width = el.clientWidth * options.scale;
   let height = el.clientHeight * options.scale;
@@ -64,6 +75,27 @@ function createWheel(el, options) {
     );
   }
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function getWeightedResult(array) {
+    let resultArray = [];
+
+    array.forEach(obj => {
+      for (let i = 0; i < obj.weight * 10; i++) {
+        resultArray.push(obj);
+      }
+    });
+
+    shuffleArray(resultArray);
+
+    return resultArray[Math.floor(Math.random() * resultArray.length)];
+  }
+
   function degreesToRadians(degrees) {
     return (degrees * Math.PI) / 180;
   }
@@ -77,10 +109,20 @@ function createWheel(el, options) {
     };
   }
 
-  function draw() {
+  function render() {
     context.clearRect(0, 0, width, height);
 
-    options.segments.forEach((segment, i) => {
+    function drawImage() {
+      const image = new Image();
+
+      image.src = options.image;
+      image.onload = () => {
+        console.log(image);
+        context.drawImage(image, 0, 0, width, height);
+      };
+    }
+
+    options.segments = options.segments.map((segment, i) => {
       const length = 360 / options.segments.length;
 
       const angles = {
@@ -88,53 +130,78 @@ function createWheel(el, options) {
         end: length * i + length,
       };
 
-      const point = polarToCartesian(angles.start, radius);
-
-      function drawSegmentShape() {
-        context.fillStyle = segment.fill;
-        context.lineWidth = strokeWidth;
-        context.strokeStyle = options.stroke.color;
-
-        context.beginPath();
-        context.arc(
-          center.x,
-          center.y,
-          radius,
-          Math.PI * (angles.start / 180),
-          Math.PI * (angles.end / 180),
-          false
-        );
-        context.lineTo(center.x, center.y);
-        context.lineTo(point.x, point.y);
-
-        context.fill();
-
-        context.stroke();
-      }
-
-      function drawSegmentText() {
-        const angle = angles.start + length / 2;
-        const point = polarToCartesian(angles.start + length / 2, radius);
-
-        context.save();
-
-        context.font = `${options.text.style} ${textSize}px ${options.text.font}`;
-        context.fillStyle = segment.color || options.text.color;
-        context.textBaseline = 'middle';
-        context.textAlign = 'right';
-
-        context.translate(point.x, point.y);
-        context.rotate(degreesToRadians(angle));
-        context.translate(-point.x, -point.y);
-
-        context.fillText(segment.text, point.x - textOffset, point.y);
-
-        context.restore();
-      }
-
-      drawSegmentShape();
-      drawSegmentText();
+      return {
+        ...segment,
+        length,
+        angles,
+      };
     });
+
+    function drawSegments() {
+      options.segments.forEach(segment => {
+        const point = polarToCartesian(segment.angles.start, radius);
+
+        function drawSegmentShape() {
+          context.fillStyle = segment.fill;
+          context.lineWidth = strokeWidth;
+          context.strokeStyle = options.stroke.color;
+
+          context.beginPath();
+          context.arc(
+            center.x,
+            center.y,
+            radius,
+            Math.PI * (segment.angles.start / 180),
+            Math.PI * (segment.angles.end / 180),
+            false
+          );
+          context.lineTo(center.x, center.y);
+          context.lineTo(point.x, point.y);
+
+          context.fill();
+
+          context.stroke();
+        }
+
+        function drawSegmentText() {
+          const angle = segment.angles.start + segment.length / 2 + 180;
+          const point = polarToCartesian(
+            segment.angles.start + segment.length / 2,
+            radius
+          );
+
+          context.save();
+
+          context.font = `${options.text.style} ${textSize}px ${options.text.font}, sans-serif`;
+          context.fillStyle = segment.color || options.text.color;
+          context.textBaseline = 'middle';
+          context.textAlign = options.text.flip ? 'right' : 'left';
+
+          context.translate(point.x, point.y);
+          context.rotate(
+            degreesToRadians(options.text.flip ? angle - 180 : angle)
+          );
+          context.translate(-point.x, -point.y);
+
+          context.fillText(
+            segment.text,
+            options.text.flip ? point.x - textOffset : point.x + textOffset,
+            point.y
+          );
+
+          context.restore();
+        }
+
+        drawSegmentShape();
+        drawSegmentText();
+      });
+    }
+
+    if (options.image) {
+      drawImage();
+    } else {
+      drawSegments();
+    }
   }
 
   function rotate(degrees) {
@@ -142,36 +209,74 @@ function createWheel(el, options) {
     context.rotate(degreesToRadians(degrees));
     context.translate(-center.x, -center.y);
 
-    draw();
+    render();
   }
+
+  let isSpinning = false;
 
   async function spin() {
-    let frame;
-    let time = new Date();
-    const end = new Date(time.getTime() + 8000);
+    return new Promise(resolve => {
+      if (isSpinning) {
+        return;
+      }
 
-    function animate() {
-      time = new Date();
-      const angle = ((2 * Math.PI) / 60) * time.getSeconds();
+      let time = new Date(),
+        angle = 0;
 
-      context.clearRect(0, 0, width, height);
+      const result = getWeightedResult(options.segments);
+      const end =
+        (options.duration / 1000) * options.speed * 360 - result.angles.start;
 
-      rotate(angle);
+      function animate() {
+        const delta = (new Date() - time) / 1000;
+        const change = options.speed * 360 * delta;
 
-      frame = window.requestAnimationFrame(animate);
-    }
+        angle = options.counterclockwise ? angle - change : angle + change;
+        angle = Math.floor(angle);
+        time = new Date();
 
-    // animate();
+        if (Math.abs(angle) >= Math.abs(end)) {
+          angle = end;
+        }
+
+        context.save();
+        rotate(angle);
+        context.restore();
+
+        if (angle === end) {
+          resolve(result);
+          isSpinning = false;
+          return;
+        }
+
+        requestAnimationFrame(animate);
+      }
+
+      animate();
+
+      isSpinning = true;
+    });
   }
 
-  draw();
+  // Vertical starting position
+  rotate(270 - 360 / options.segments.length / 2);
+
+  if (options.text.fontHref) {
+    const image = new Image();
+
+    image.src = options.text.fontHref;
+    image.onerror = render;
+  }
 
   return {
-    el,
     canvas,
-    draw,
+    el,
+    options,
+    render,
     rotate,
     spin,
+    shuffleArray,
+    getWeightedResult,
     degreesToRadians,
     polarToCartesian,
   };
